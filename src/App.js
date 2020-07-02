5385572f18befb89c1fc7ad9777b373c921a0144
aimalhuma61 <66478525+aimalhuma61@users.noreply.github.com>
2020-07-02 13:19:28 +0500

    Add Api and info panel

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar1 from './Components/NavBar'
+import InfoPanel from './Components/InfoPanel'
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar1/>
+    <InfoPanel/>
+    </div>
+  );
+}
+
+export default App;
